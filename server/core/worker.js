068c894be24fee28cd51bbbb565dfaf7ae5e8445
max <max@jigsaw.xyz>
2021-05-07 16:03:48 +0100

    semicolon on wrong line

--- server/core/worker.js
+++ server/core/worker.js
@@ -11,9 +11,9 @@ global.WIKI = WIKI
 
 WIKI.configSvc.init()
 WIKI.logger = require('./logger').init('JOB')
-const args = require('yargs').argv
+const args = require('yargs').argv;
 
-;(async () => {
+(async () => {
   await require(`../jobs/${args.job}`)(args.data)
   process.exit(0)
 })()
